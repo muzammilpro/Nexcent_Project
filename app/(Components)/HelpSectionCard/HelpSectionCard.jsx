5e9eafb612b5d1ffde5850a044d08675638bff04
muzammilpro <muzammilhusnain7@gmail.com>
2023-01-05 10:55:02 +0500

    Under Procedure

--- app/(Components)/HelpSectionCard/HelpSectionCard.jsx
+++ app/(Components)/HelpSectionCard/HelpSectionCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Image from 'next/image'
+import './HelpSectioCard.css'
 
 export default function HelpSectionCard(props) {
   return (
     <div className='HelpCardParent'>
       <div>
-        <Image src={props.src} />
+        <Image  src={props.src} />
       </div>
       <div>
         <p className='Heading'>2,245,341</p>
