40e4afab87799bb7326da2b51b37b7716fc2f439
muzammilpro <muzammilhusnain7@gmail.com>
2023-10-12 22:59:13 +0500

    Under Procedure

--- app/(Components)/Navbar/Navbar.jsx
+++ app/(Components)/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import React from 'react'
 import Logo from '../../../public/Assets/logo.jpg'
+import Link from 'next/link'
+import './Navbar.css'
 
 
 export default function Navbar() {
@@ -14,16 +16,17 @@ export default function Navbar() {
                         height={50}
                         alt="Picture of the author"
                     />
-                    
+
                     <span class="ml-3 text-xl">Nexcent</span>
                 </a>
                 <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                    <a class="mr-5 hover:text-gray-900">First Link</a>
-                    <a class="mr-5 hover:text-gray-900">Second Link</a>
-                    <a class="mr-5 hover:text-gray-900">Third Link</a>
-                    <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+                    <Link className='anchorTag ' href="/">Home</Link>
+                    <Link className='anchorTag' href="/">Features</Link>
+                    <Link className='anchorTag' href="/">Community</Link>
+                    <Link className='anchorTag' href="/">Blog</Link>
+                    <Link className='anchorTag' href="/">Pricing</Link>
                 </nav>
-                <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+                <button class="inline-flex items-center bg-lime-400 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Register Now
                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
                         <path d="M5 12h14M12 5l7 7-7 7"></path>
                     </svg>
