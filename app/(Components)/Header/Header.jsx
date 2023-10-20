bc6f9593b1a59cff430e60c41da22277d5472946
muzammilpro <muzammilhusnain7@gmail.com>
2023-10-21 00:21:59 +0500

    Under Procedure

--- app/(Components)/Header/Header.jsx
+++ app/(Components)/Header/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
                 <p>Lessons and insight</p>
                 <p>From 8 Years</p>
                 <p>where to grow your bussiness as a photographer: Siter or Social media</p>
-                <Button text=""/>
+                <Button text="Register"/>
             </div>
             <div>
 
