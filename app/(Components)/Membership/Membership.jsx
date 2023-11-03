4625c56a8d88194958a15e63a81dd9275918e41d
muzammilpro <muzammilhusnain7@gmail.com>
2023-11-03 12:43:41 +0500

    Under Procedure

--- app/(Components)/Membership/Membership.jsx
+++ app/(Components)/Membership/Membership.jsx
@@ -9,7 +9,7 @@ export default function Membership() {
         <p>Who is Nexcent suitable for?</p>
       </center>
       <div>
-        <Card  heading="" />
+        <Card  heading="" text=""/>
       </div>
     </div>
   )
