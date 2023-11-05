de9012541e39434499ee951b6009187c5dbb8a40
muzammilpro <muzammilhusnain7@gmail.com>
2023-11-05 12:45:05 +0500

    Under Procedure

--- app/(Components)/Membership/Membership.jsx
+++ app/(Components)/Membership/Membership.jsx
@@ -11,6 +11,7 @@ export default function Membership() {
       <div>
         <Card  heading="" text=""/>
         <Card  heading="" text=""/>
+        <Card  heading="" text=""/>
       </div>
     </div>
   )
