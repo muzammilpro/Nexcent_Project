ad587a1e58fd30663c4c07b313081c76358fb29c
muzammilpro <muzammilhusnain7@gmail.com>
2023-11-30 22:58:04 +0500

    Under Procedure

--- app/(Components)/BigCard/BigCard.jsx
+++ app/(Components)/BigCard/BigCard.jsx
@@ -9,7 +9,7 @@ export default function BigCard() {
             </div>
             <div>
                 <p>The Unseen of spending three year at Pixelgrade</p>
-                <p></p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam voluptates beatae corrupti hic quaerat recusandae provident ad nesciunt quasi vitae ea modi itaque voluptatibus eos magnam, alias mollitia laudantium similique. </p>
             </div>
         </div>
     )
