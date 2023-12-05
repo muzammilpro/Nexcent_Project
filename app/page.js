f0884644220db426bcb1ff01b0acafb07358b669
muzammilpro <muzammilhusnain7@gmail.com>
2023-12-05 09:59:53 +0500

    Under Procedure

--- app/page.js
+++ app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
       <OurClients />
       <Membership />
       <br />
-      <BigCard heading="The Unseen of spending three year at Pixelgrade" img={CardImage1} />
+      <BigCard heading="The Unseen of spending three year at Pixelgrade" text="" img={CardImage1} />
 
     </>
   )
