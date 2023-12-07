79ccdfbf589d66b3888d39c17270bf8d671d9d81
muzammilpro <muzammilhusnain7@gmail.com>
2023-12-07 10:01:52 +0500

    Under Procedure

--- app/(Components)/BigCard/BigCard.jsx
+++ app/(Components)/BigCard/BigCard.jsx
@@ -10,7 +10,7 @@ export default function BigCard(props) {
                 <Image src={props.img} className='cardImage' />
             </div>
             <div className='textDiv'>
-                <p className='Heading'>The Unseen of spending three year at Pixelgrade</p> <br />
+                <p className='Heading'>{props.heading}</p> <br />
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam voluptates beatae corrupti hic quaerat recusandae provident ad nesciunt quasi vitae ea modi itaque voluptatibus eos magnam, alias mollitia laudantium similique. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque temporibus illo velit officia, non assumenda repudiandae aliquid quis aspernatur placeat. Ea sapiente libero repellat amet sequi, enim ratione quam.</p>
                 <br />
                 <Button text="Learn More" />
