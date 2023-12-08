bd5ef8f94b39459bee6cb6edde960b0cff5e3282
muzammilpro <muzammilhusnain7@gmail.com>
2023-12-08 10:02:37 +0500

    Under Procedure

--- app/(Components)/BigCard/BigCard.jsx
+++ app/(Components)/BigCard/BigCard.jsx
@@ -11,7 +11,7 @@ export default function BigCard(props) {
             </div>
             <div className='textDiv'>
                 <p className='Heading'>{props.heading}</p> <br />
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam voluptates beatae corrupti hic quaerat recusandae provident ad nesciunt quasi vitae ea modi itaque voluptatibus eos magnam, alias mollitia laudantium similique. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque temporibus illo velit officia, non assumenda repudiandae aliquid quis aspernatur placeat. Ea sapiente libero repellat amet sequi, enim ratione quam.</p>
+                <p>{props.text}</p>
                 <br />
                 <Button text="Learn More" />
             </div>
