247ffd029420a9cd523d3539c8bb5ce3cb00633a
muzammilpro <muzammilhusnain7@gmail.com>
2023-12-13 10:08:42 +0500

    Under Procedure

--- app/(Components)/HelpSection/HelpSection.jsx
+++ app/(Components)/HelpSection/HelpSection.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 export default function HelpSection() {
-  return (
-    <div>
-      <div></div>
-      <div></div>
-    </div>
-  )
+    return (
+        <div>
+            <div></div>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
 }
