a6c0cda1e124ca16a18ac80881bc00b7fc3ec3f6
muzammilpro <muzammilhusnain7@gmail.com>
2023-12-22 10:28:53 +0500

    Under Procedure

--- app/(Components)/HelpSection/HelpSection.jsx
+++ app/(Components)/HelpSection/HelpSection.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import HelpSectionCard from '../HelpSectionCard/HelpSectionCard'
 
 export default function HelpSection() {
     return (
         <div>
             <div></div>
             <div>
-                
+                <HelpSectionCard numbers="2,245,341"/>
             </div>
         </div>
     )
