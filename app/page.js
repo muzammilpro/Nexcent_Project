de8dbc39fed016385e663941a4d6a2e0371bd884
muzammilpro <muzammilhusnain7@gmail.com>
2023-12-27 10:35:59 +0500

    Under Procedure

--- app/page.js
+++ app/page.js
@@ -13,7 +13,8 @@ export default function Home() {
       <Membership />
       <br />
       <BigCard heading="The Unseen of spending three year at Pixelgrade" text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam voluptates beatae corrupti hic quaerat recusandae provident ad nesciunt quasi vitae ea modi itaque voluptatibus eos magnam, alias mollitia laudantium similique. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque temporibus illo velit officia, non assumenda repudiandae aliquid quis aspernatur placeat. Ea sapiente libero repellat amet sequi, enim ratione quam." img={CardImage1} />
-
+       <br />
+       
     </>
   )
 }
