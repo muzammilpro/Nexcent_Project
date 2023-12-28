0021cab92b9fd22671c969442b80807a1dd6d9b4
muzammilpro <muzammilhusnain7@gmail.com>
2023-12-28 10:37:18 +0500

    Under Procedure

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import OurClients from './(Components)/OurClients/OurClients'
 import Membership from './(Components)/Membership/Membership'
 import BigCard from './(Components)/BigCard/BigCard'
 import CardImage1 from '../public/Assets/InputForm.jpg'
+import HelpSection from './(Components)/HelpSection/HelpSection'
 
 export default function Home() {
   return (
@@ -14,7 +15,8 @@ export default function Home() {
       <br />
       <BigCard heading="The Unseen of spending three year at Pixelgrade" text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam voluptates beatae corrupti hic quaerat recusandae provident ad nesciunt quasi vitae ea modi itaque voluptatibus eos magnam, alias mollitia laudantium similique. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque temporibus illo velit officia, non assumenda repudiandae aliquid quis aspernatur placeat. Ea sapiente libero repellat amet sequi, enim ratione quam." img={CardImage1} />
        <br />
-       
+       <HelpSection/>
+
     </>
   )
 }
