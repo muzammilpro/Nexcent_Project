b559c3aa4600a88c9b2a917e009218dc1d728cb2
muzammilpro <muzammilhusnain7@gmail.com>
2024-01-09 00:19:36 +0500

    Under Procedure

--- app/(Components)/Header/Header.jsx
+++ app/(Components)/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Button from '../Button/Button'
 
 export default function Header() {
     return (
@@ -7,7 +8,7 @@ export default function Header() {
                 <p>Lessons and insight</p>
                 <p>From 8 Years</p>
                 <p>where to grow your bussiness as a photographer: Siter or Social media</p>
-                
+                <Button/>
             </div>
             <div>
 
