16f31c1b88fc0f5b29a55ea8a6f09b2746e52fd8
muzammilpro <muzammilhusnain7@gmail.com>
2024-01-11 00:18:02 +0500

    Under Procedure

--- app/(Components)/Membership/Membership.jsx
+++ app/(Components)/Membership/Membership.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 export default function Membership() {
   return (
     <div>
-        <p>Mange your entire community in a single system</p>
-        <p></p>
-      
+    <p>Manage your entire community in a single system</p>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Header from './(Components)/Header/Header'
 import OurClients from './(Components)/OurClients/OurClients'
+import Membership from './(Components)/Membership/Membership'
 
 export default function Home() {
   return (
  <>
  <Header/>
- <OurClients/>
+ <OurClients/> <br />
+ <Membership/>
 
  </>
   )
