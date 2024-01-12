083ff0eed05287c03298bfa12cf6d206dd34cecf
muzammilpro <muzammilhusnain7@gmail.com>
2024-01-12 09:52:51 +0500

    Under Procedure

--- app/(Components)/BigCard/BigCard.jsx
+++ app/(Components)/BigCard/BigCard.jsx
@@ -1,15 +1,19 @@
 import Image from 'next/image'
 import React from 'react'
+import './BigCard.css'
+import Button from '../Button/Button'
 
-export default function BigCard() {
+export default function BigCard(props) {
     return (
-        <div>
-            <div>
-
+        <div className='ParentDiv'>
+            <div className='imageDiv'>
+                <Image src={props.img} className='cardImage' />
             </div>
-            <div>
-                <p>The Unseen of spending three year at Pixelgrade</p>
+            <div className='textDiv'>
+                <p className='Heading'>The Unseen of spending three year at Pixelgrade</p> <br />
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam voluptates beatae corrupti hic quaerat recusandae provident ad nesciunt quasi vitae ea modi itaque voluptatibus eos magnam, alias mollitia laudantium similique. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab atque temporibus illo velit officia, non assumenda repudiandae aliquid quis aspernatur placeat. Ea sapiente libero repellat amet sequi, enim ratione quam.</p>
+                <br />
+                <Button text="Learn More" />
             </div>
         </div>
     )
--- app/page.js
+++ app/page.js
@@ -2,15 +2,18 @@ import Image from 'next/image'
 import Header from './(Components)/Header/Header'
 import OurClients from './(Components)/OurClients/OurClients'
 import Membership from './(Components)/Membership/Membership'
+import BigCard from './(Components)/BigCard/BigCard'
+import CardImage1 from '../public/Assets/InputForm.jpg'
 
 export default function Home() {
   return (
- <>
- <Header/>
- <OurClients/> 
- <Membership/>
- <br />
+    <>
+      <Header />
+      <OurClients />
+      <Membership />
+      <br />
+      <BigCard img={CardImage1} />
 
- </>
+    </>
   )
 }
